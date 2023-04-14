b81508a04f4dd234a42b8c404bb7a0161aaebe7f
0xukhezo <ukhezo.web3@gmail.com>
2023-04-14 18:57:05 +0200

    feat:change the approve button to father component

--- src/components/CreateCampaingButton.tsx
+++ src/components/CreateCampaingButton.tsx
@@ -15,12 +15,20 @@ interface EventIdInputInterface {
   amountInSMC: number;
   amountFlowRate: number;
   clientInfo: string;
+  txLoadingApprove: boolean;
+  txErrorApprove: boolean;
+  txSuccessApprove: boolean;
+  dataApproveHash: `0x${string}` | undefined;
 }
 
 export default function CreateCampaingButton({
   amountInSMC,
   amountFlowRate,
   clientInfo,
+  txSuccessApprove,
+  txLoadingApprove,
+  txErrorApprove,
+  dataApproveHash,
 }: EventIdInputInterface) {
   const [lensProfile, setLensProfile] = useState<any>();
   const [body, setBody] = useState<any>();
@@ -52,27 +60,8 @@ export default function CreateCampaingButton({
     args: [amount],
   });
 
-  const { config: approveTokensContractConfig } = usePrepareContractWrite({
-    address: "0xbe49ac1EadAc65dccf204D4Df81d650B50122aB2",
-    abi: abi.abiFUSDC,
-    functionName: "approve",
-    args: [campaignsFactoryAddress, amount],
-  });
-
-  const { writeAsync: ApproveTokensContractTx, data: dataApprove } =
-    useContractWrite(approveTokensContractConfig);
   const { writeAsync: createCampaignContractTx, data: dataCampaign } =
     useContractWrite(createCampaignContractConfig);
-
-  const {
-    data: txReceiptApprove,
-    isSuccess: txSuccessApprove,
-    isError: txErrorApprove,
-    isLoading: txLoadingApprove,
-  } = useWaitForTransaction({
-    confirmations: 10,
-    hash: dataApprove?.hash,
-  });
 
   const {
     isSuccess: txSuccessCampaign,
@@ -239,14 +228,6 @@ export default function CreateCampaingButton({
     }
   }
 
-  const onSendClick = async () => {
-    try {
-      await ApproveTokensContractTx?.();
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   const onCreateClick = async () => {
     try {
       await createCampaignContractTx?.();
@@ -294,7 +275,7 @@ export default function CreateCampaingButton({
   useEffect(() => {
     if (txLoadingApprove || txLoadingCampaign) {
       if (txLoadingApprove) {
-        setHash(dataApprove?.hash);
+        setHash(dataApproveHash);
       } else {
         setHash(dataCampaign?.hash);
       }
@@ -308,7 +289,7 @@ export default function CreateCampaingButton({
   useEffect(() => {
     if (txSuccessApprove || txSuccessCampaign) {
       if (txSuccessApprove) {
-        setHash(dataApprove?.hash);
+        setHash(dataApproveHash);
         setType("success");
         setMessage("USDC approved!");
       }
@@ -323,7 +304,7 @@ export default function CreateCampaingButton({
   useEffect(() => {
     if (txErrorCampaign || txErrorApprove) {
       if (txErrorApprove) {
-        setHash(dataApprove?.hash);
+        setHash(dataApproveHash);
       } else {
         setHash(dataCampaign?.hash);
       }
@@ -355,47 +336,6 @@ export default function CreateCampaingButton({
             message={"This address is not owner of a Lens Profile"}
             getCloseAlert={getCloseAlert}
           />
-        ) : txReceiptApprove === undefined ? (
-          amountInSMC === 0 || amountFlowRate === 0 ? (
-            <div className="mt-5 flex justify-center ">
-              <div className="px-20 py-5 rounded-full text-gray-600 bg-gray-200 leading-8 font-bold opacity-50 tracking-wide">
-                Approve USDC
-              </div>
-            </div>
-          ) : !txLoadingApprove ? (
-            <div className="mt-5 flex justify-center ">
-              <button
-                onClick={() => onSendClick()}
-                className=" px-20 py-5 rounded-full bg-superfluid-100 leading-8 font-bold tracking-wide"
-              >
-                Approve USDC
-              </button>
-            </div>
-          ) : (
-            <div className="mt-5 flex justify-center ">
-              <div className=" px-20 py-5 rounded-full bg-superfluid-100 leading-8 font-bold tracking-wide flex">
-                <div role="status">
-                  <svg
-                    aria-hidden="true"
-                    className="w-8 h-8 mr-2 text-gray-500 animate-spin dark:text-gray-600 fill-white"
-                    viewBox="0 0 100 101"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                      fill="currentFill"
-                    />
-                  </svg>
-                </div>
-                Approving
-              </div>
-            </div>
-          )
         ) : amountInSMC === 0 || amountFlowRate === 0 ? (
           <div className="mt-5 flex justify-center ">
             <div className="px-20 py-5 rounded-full text-gray-600 bg-gray-200 leading-8 font-bold opacity-50 tracking-wide">
