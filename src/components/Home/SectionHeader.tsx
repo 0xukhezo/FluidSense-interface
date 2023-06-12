c858b2c9390e1f92ab1991404096d9f955f2f889
0xUkhezo <ukhezo.web3@gmail.com>
2023-06-12 10:46:36 +0200

    fix:Resolve problem with '

--- src/components/Home/SectionHeader.tsx
+++ src/components/Home/SectionHeader.tsx
@@ -19,7 +19,7 @@ export default function SectionHeader() {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2">
             <button className="text-xl font-semibold text-black lg:px-10 py-4 rounded-full  bg-superfluid-100 flex items-center leading-6 lg:min-w-[250px] lg:min-h-[80px] justify-center max-w-[250px]">
-              Let's go!
+              Let <span>{"'"}</span>s go!
             </button>
             <button className="mt-6 md:mt-0 md:ml-10 text-xl border-1 border-superfluid-100 text-superfluid-100 py-4 rounded-full bg-superfluid-200 flex items-center font-bold text-xl leading-6 lg:min-w-[250px] lg:min-h-[52px] justify-center max-w-[250px]">
               Learn more
